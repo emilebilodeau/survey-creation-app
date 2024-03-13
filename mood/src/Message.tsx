4b2c49abb2f9b478716fc08d975abf616ec892cd
ALDC-emile-bilodeau <emile.bilodeau@aldc.io>
2024-03-13 16:41:47 +1100

    deleted most of default app to start from scratch

---
+++ mood/src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing 
+// NOTE: this is a function component, which has become more popular recently...
+// NOTE: the older way of doing it uses class components
+function Message () {
+    // JSX: JavaScript XML
+    const name = 'Emile';
+    return <h1>Hello {name}</h1>;
+}
+
+export default Message;
