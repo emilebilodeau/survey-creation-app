a1cd3c9ab9eb24762963542255b47efc673dd967
emilebilodeau <emilebilodeau@gmail.com>
2024-03-17 17:25:13 +1100

    added basic routing

--- mood/src/App.tsx
+++ mood/src/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
-import Temp from './components/temp';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Form from './pages/Form';
 
 function App() {
 
   return (
-    <div>
-      <Temp />
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='form' element={<Form/>} ></Route>
+    </Routes>
   )
 }
 
--- mood/src/components/Temp.tsx
+++ mood/src/components/Temp.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// components in this general components directory are meant to be 
+// reusable across the app
+
 const Temp = () => {
   return (
     <div>Temporary</div>
