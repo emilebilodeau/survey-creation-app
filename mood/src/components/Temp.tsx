6bb8756149eff2cefc202eb8e9033b22c3f6a3be
Emile Bilodeau <76504538+emilebilodeau@users.noreply.github.com>
2024-03-24 16:11:39 +1100

    Revert "Navbar"

---
+++ mood/src/components/Temp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// components in this general components directory are meant to be 
+// reusable across the app
+
+const Temp = () => {
+  return (
+    <div>Temporary</div>
+  )
+}
+
+export default Temp
