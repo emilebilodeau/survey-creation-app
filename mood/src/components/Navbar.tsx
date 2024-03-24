cd5b7dca989edac1345758f8f170b068e56069ea
emilebilodeau <emilebilodeau@gmail.com>
2024-03-24 16:46:32 +1100

    completed the basics of the main navbar

---
+++ mood/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav id="main-navbar" className="nav">
+      <Link to="/" className="site-title">
+        Home
+      </Link>
+      <ul>
+        <CustomLink to="/form">Form</CustomLink>
+        <CustomLink to="/data">Data</CustomLink>
+      </ul>
+    </nav>
+  );
+};
+
+// this function is essentially just used to add the active class to the Link element
+// of the current page. it uses react hooks to retrieve current url
+// TODO: come back and do the type assignment correctly. also include props as an argument
+function CustomLink({ to, children }: { to: any; children: any }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to}>{children}</Link>
+    </li>
+  );
+}
+
+export default Navbar;
---
+++ mood/src/pages/Data.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Data = () => {
+  return <h1>Data</h1>;
+};
+
+export default Data;
