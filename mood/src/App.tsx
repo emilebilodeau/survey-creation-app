b19fc8aeb6368c6d2a83470001020843e7019c8c
emilebilodeau <emilebilodeau@gmail.com>
2024-12-02 16:22:35 -0800

    made data table larger

--- mood/src/App.tsx
+++ mood/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="container">
+      <div className="container-fluid">
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/form" element={<Form />}></Route>
